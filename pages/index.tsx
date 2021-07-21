47c482b3f0d51bb0b0480d8794ae32598db24dec
Lemuel Lancaster <32617101+Itsakaseru@users.noreply.github.com>
2021-07-21 16:53:44 +0700

    Add Head component for homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,24 +7,32 @@ import QuickNav from "../components/quick-nav";
 export default function Home()
 {
 	return (
-		<div className="flex flex-col justify-between h-screen">
-			<Navbar />
-			<div className="self-center max-w-lg rounded-full">
-				<Image
-					src={ profilePicture }
-					className="rounded-full"
-					alt="Itsakaseru profile picture"
-				/>
-			</div>
-			<QuickNav />
-			<footer className="flex justify-between mx-10 my-8">
-				<div className="font-secondary font-semibold text-primary">
-					Profile picture by <a className="hover:text-primary-light" href="https://www.pixiv.net/member.php?id=7238253">fishy</a> (I edited the hair and eye)
+		<>
+			<Head>
+				<title>Itsakaseru</title>
+				<meta name="description" content="Itsakaseru Personal Website"></meta>
+				<meta name="author" content="Remueru Itsakaseru"></meta>
+				<meta name="viewport" content="initial-scale=1.0, width=device-width" />
+			</Head>
+			<div className="flex flex-col justify-between h-screen">
+				<Navbar />
+				<div className="self-center max-w-lg rounded-full">
+					<Image
+						src={ profilePicture }
+						className="rounded-full"
+						alt="Itsakaseru profile picture"
+					/>
 				</div>
-				<div className="font-secondary font-semibold text-primary">
-					© { new Date().getFullYear() } Itsakaseru. All Rights Reserved
-				</div>
-			</footer>
-		</div >
+				<QuickNav />
+				<footer className="flex justify-between mx-10 my-8">
+					<div className="font-secondary font-semibold text-primary">
+						Profile picture by <a className="hover:text-primary-light" href="https://www.pixiv.net/member.php?id=7238253">fishy</a> (I edited the hair and eye)
+					</div>
+					<div className="font-secondary font-semibold text-primary">
+						© { new Date().getFullYear() } Itsakaseru. All Rights Reserved
+					</div>
+				</footer>
+			</div >
+		</>
 	);
 }
