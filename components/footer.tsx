58bb7398510462518fe177f01a30ba8e68321e0e
Lemuel Lancaster <32617101+Itsakaseru@users.noreply.github.com>
2021-07-24 15:03:10 +0700

    Change footer component to use padding instead of margin

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer()
 {
     return (
-        <footer className="flex justify-between mx-10 my-8">
+        <footer className="flex justify-between px-10 py-8">
             <div className="font-secondary font-semibold text-primary">
                 Profile picture by <a className="hover:text-primary-light" href="https://www.pixiv.net/member.php?id=7238253">fishy</a> (I edited the hair and eye)
             </div>
