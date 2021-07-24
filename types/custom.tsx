9314ba0460ba85f2423bf20ab1c79ce194030e05
Lemuel Lancaster <32617101+Itsakaseru@users.noreply.github.com>
2021-07-24 15:05:10 +0700

    Add custom typescript type for portfolio data

---
+++ types/custom.tsx
@@ -0,0 +1,30 @@
+import { IconProp } from "@fortawesome/fontawesome-svg-core";
+
+interface IPortfolioDcs
+{
+    name: string,
+    icon: IconProp,
+    link?: string
+}
+
+interface IPortfolioImg
+{
+    type: string,
+    alt: string,
+    src: string,
+    blurSrc: string,
+    width: number,
+    height: number
+}
+
+export interface IPortfolio
+{
+    id: string,
+    name: string,
+    icon: StaticImageData,
+    color: string,
+    desc: string,
+    longDesc: string,
+    dcs: IPortfolioDcs[],
+    img: IPortfolioImg[]
+}
