377e6c01840e422f2c89a23d8bacc0cc9859f568
Itsakaseru <32617101+Itsakaseru@users.noreply.github.com>
2022-02-08 20:12:40 +0700

    Add redirect from `/saweria` to `https://saweria.co/itsakaseru`

--- next.config.js
+++ next.config.js
@@ -1,3 +1,12 @@
 module.exports = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: '/saweria',
+        destination: 'https://saweria.co/itsakaseru',
+        permanent: false,
+      },
+    ]
+  },
 }
