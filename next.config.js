f4d7a814dd4ed4cc050ca10bec9ef7d807ddcecf
Lemuel Lancaster <32617101+Itsakaseru@users.noreply.github.com>
2022-05-08 21:04:26 +0700

    Add redirection link onKeys/survey to gform

--- next.config.js
+++ next.config.js
@@ -7,6 +7,11 @@ module.exports = {
         destination: 'https://saweria.co/itsakaseru',
         permanent: false,
       },
+      {
+        source: '/onKeys/survey',
+        destination: 'https://docs.google.com/forms/d/e/1FAIpQLScMtN3W2ZULGf_PeBo1hjO9eWxONn21TEyEMpK_0EPSxqp-5Q/viewform?usp=sf_link',
+        permanent: false,
+      }
     ]
   },
 }
