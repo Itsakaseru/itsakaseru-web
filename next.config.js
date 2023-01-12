21ba5f6473e868b0a4a865f65959c77a03cec543
Itsakaseru <32617101+Itsakaseru@users.noreply.github.com>
2023-01-12 23:14:19 +0700

    Add security header

--- next.config.js
+++ next.config.js
@@ -6,6 +6,43 @@ module.exports = {
   experimental: {
     appDir: true,
   },
+  async headers() {
+    const ContentSecurityPolicy = `
+      default-src 'self';
+      script-src 'self' 'unsafe-eval' 'unsafe-inline' https://www.googletagmanager.com https://www.google-analytics.com https://ssl.google-analytics.com;
+      style-src 'self' 'unsafe-inline';
+      img-src 'self' www.googletagmanager.com https://www.google-analytics.com;
+      connect-src 'self' https://www.google-analytics.com;
+      font-src 'self';
+    `
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'X-Frame-Options',
+            value: 'SAMEORIGIN'
+          },
+          {
+            key: 'X-XSS-Protection',
+            value: '1; mode=block'
+          },
+          {
+            key: 'Content-Security-Policy',
+            value: ContentSecurityPolicy.replace(/\s{2,}/g, ' ').trim()
+          },
+          {
+            key: 'Referrer-Policy',
+            value: 'strict-origin-when-cross-origin'
+          },
+          {
+            key: 'Permissions-Policy',
+            value: 'camera=(), microphone=(), geolocation=(), browsing-topics=()'
+          }
+        ]
+      }
+    ]
+  },
   async redirects() {
     return [
       {
