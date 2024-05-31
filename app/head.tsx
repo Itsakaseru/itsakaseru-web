5901598a657a2b2e955e42d1e2acdc52d32f26b8
Itsakaseru <sakii@itsakaseru.me>
2024-06-01 01:40:58 +0700

    Reset for rewrites

--- app/head.tsx
+++
@@ -1,9 +0,0 @@
-export default function Head() {
-  return (
-    <>
-			<title>Itsakaseru</title>
-			<meta name="description" content="Itsakaseru Personal Website" />
-			<meta name="viewport" content="initial-scale=1.0, width=device-width" />
-    </>
-  )
-}
--- utils/common.ts
+++
@@ -1,9 +0,0 @@
-export function disableScroll()
-{
-    document.body.style.overflow = "hidden";
-}
-
-export function enableScroll()
-{
-    document.body.style.overflow = "visible";
-}
