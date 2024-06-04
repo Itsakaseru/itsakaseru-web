c7133b65568d5b4f20e4a05884e1bf47b149dfee
Itsakaseru <sakii@itsakaseru.me>
2024-06-05 03:53:42 +0700

    Add homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,112 +1,134 @@
+"use client";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import { faYoutube, faGithub, faXTwitter, faLinkedin } from "@fortawesome/free-brands-svg-icons";
+import { CodeBracketSquareIcon, NewspaperIcon, Square2StackIcon, UserCircleIcon } from "@heroicons/react/24/solid";
 import Image from "next/image";
+import { motion } from "framer-motion";
+import {useState} from "react";
 
 export default function Home() {
+  const WEB_LINKS = [
+    {
+      icon: CodeBracketSquareIcon,
+      href: "",
+      name: "Portfolio",
+    },
+    {
+      icon: UserCircleIcon,
+      href: "",
+      name: "About Me"
+    },
+    {
+      icon: NewspaperIcon,
+      href: "",
+      name: "My Blog"
+    },
+    {
+      icon: Square2StackIcon,
+      href: "",
+      name: "Fun"
+    }
+  ];
+
+  const SOCIAL_LINKS = [
+    {
+      icon: faEnvelope,
+      href: "mailto:contact@itsakaseru.me",
+      name: "Mail",
+    },
+    {
+      icon: faLinkedin,
+      href: "https://linkedin.com/in/itsakaseru",
+      name: "LinkedIn"
+    },
+    {
+      icon: faYoutube,
+      href: "https://youtube.com/c/Itsakaseru",
+      name: "YouTube"
+    },
+    {
+      icon: faGithub,
+      href: "https://github.com/Itsakaseru",
+      name: "GitHub"
+    },
+    {
+      icon: faXTwitter,
+      href: "https://x.com/Itsakaseru",
+      name: "X Formerly Twitter"
+    }
+  ];
+  
+  const [ selectedTab, setSelectedTab ] = useState<string | null>(WEB_LINKS[0].name);
+  
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className="items-center h-screen p-16">
+      <div className="flex flex-col justify-between w-full h-full rounded-3xl bg-white p-14">
+        <nav className="flex flex-row justify-between">
+          <div className="flex flex-col text-cocoa -space-y-1">
+            <div className="text-3xl font-semibold">Itsakaseru</div>
+            <div className="text-base font-extralight">Lemuel Lancaster</div>
+          </div>
+          <div className="flex flex-row items-center space-x-8">
+            {
+              SOCIAL_LINKS.map((link) =>
+                <a key={link.name} href={link.href}><FontAwesomeIcon icon={link.icon} className="size-6 text-cocoa transition-colors hover:text-cocoa-dark duration-300"/></a>
+              )
+            }
+          </div>
+        </nav>
+        <div className="flex flex-col space-y-12">
+          <Image
+            className="mx-auto rounded-[3.125rem]"
+            src="/sakii.png"
+            width={350}
+            height={350}
+            alt="Photo of Sakii as Anime Character"
+          />
+          <div className="mx-auto text-cocoa">
+            <motion.div
+              className="flex flex-row space-x-3 rounded-xl bg-white-light p-4"
+              layout
+              transition={{duration: 0.25, type: "tween", stiffness: 50}}
+            >
+              {
+                WEB_LINKS.map((link) =>
+                  <motion.a
+                    key={link.name}
+                    className={`flex flex-row px-3 py-2 space-x-2 transition-colors duration-300 ${link.name === selectedTab && "bg-white rounded hover:bg-white-dark"}`}
+                    href="https://itsakaseru.me/portfolio"
+                    layout
+                    onHoverStart={() => setSelectedTab(link.name)}
+                  >
+                    <motion.div layout="position">
+                      <link.icon className="size-6"/>
+                    </motion.div>
+                    {
+                      link.name === selectedTab &&
+                        <motion.p initial={{ opacity: 0 }} animate={{ opacity: 1}}>
+                          { link.name }
+                        </motion.p>
+                    }
+                  </motion.a>
+                )
+              }
+            </motion.div>
+          </div>
         </div>
-      </div>
-
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+        <footer className="mx-auto text-cocoa">
+          © 2024<ruby> It
+          <rt>イ</rt>
+            sa
+          <rt>サ</rt>
+            ka
+          <rt>カ</rt>
+            se
+          <rt>セ</rt>
+            ru
+          <rt>ル</rt>
+        </ruby>. All Rights Reserved.
+        </footer>
       </div>
     </main>
   );
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,12 +8,19 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-    },
+      colors: {
+        "cocoa" : {
+          light: "#F4EAE9",
+          DEFAULT: "#694B47",
+          dark: "#452520",
+        },
+        "white": {
+          light: "#FFFFFF",
+          DEFAULT: "#F9F9F9",
+          dark: "#F5F5F5",
+        }
+      }
+    }
   },
   plugins: [],
 };
