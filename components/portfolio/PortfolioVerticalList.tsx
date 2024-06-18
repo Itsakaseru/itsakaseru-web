75d44d3ce94f0a03e1a9920c9d55d19f57d44012
Itsakaseru <sakii@itsakaseru.me>
2024-06-18 16:34:13 +0700

    Decouple portfolio vertical list and show current portfolio on top

---
+++ components/portfolio/PortfolioVerticalList.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import Portfolio, { IPortfolioMetadata } from "@/components/Portfolio";
+import { motion, AnimatePresence } from "framer-motion";
+
+export default function PortfolioVerticalList({ portfolios, currentSlug }: { portfolios: IPortfolioMetadata[], currentSlug: string }) {
+  return (
+    <AnimatePresence>
+      <motion.div className="flex flex-col h-[70vh] overflow-y-auto overflow-hidden items-center p-6 bg-white-light outline outline-1 outline-cocoa-light rounded-xl gap-4">
+        {
+          portfolios.map((portfolio) => (
+            <motion.div
+              key={portfolio.name}
+              className="self-stretch max-w-96"
+            >
+              <Portfolio
+                portfolio={portfolio}
+                options={portfolio.slug == currentSlug ? { mode: "background", showDescription: true } : {
+                  mode: "none",
+                  showDescription: false
+                }}
+              />
+            </motion.div>
+          ))
+        }
+      </motion.div>
+    </AnimatePresence>
+  );
+}
