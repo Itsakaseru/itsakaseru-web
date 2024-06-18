69d621158174678ccfa332022d48a651d8750cd2
Itsakaseru <sakii@itsakaseru.me>
2024-06-18 16:34:38 +0700

    Ignore overflow in markdown element

--- components/Markdown.tsx
+++ components/Markdown.tsx
@@ -1,11 +1,12 @@
-import { MDXRemote, MDXRemoteSerializeResult } from "next-mdx-remote/rsc";
+import { MDXRemote } from "next-mdx-remote/rsc";
 import {IPortfolio} from "@/components/Portfolio";
 import InfoBubble from "@/components/InfoBuble";
+import Gallery from "@/components/Gallery";
 
 export default function Markdown({ content, metadata } : IPortfolio) {
   return (
-    <article className={ `resize-none max-h-full overflow-y-scroll overflow-x-hidden text-${ metadata.color }-dark` }>
-      <MDXRemote source={ content } components={{ InfoBubble }}/>
+    <article className={ `overflow-x-visible text-${ metadata.color }-dark` }>
+      <MDXRemote source={ content } components={{ InfoBubble, Gallery }}/>
     </article>
   );
 }
