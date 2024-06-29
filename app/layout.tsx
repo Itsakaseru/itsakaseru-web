bf229d58d19ee5eba2292860562dffbe9a2b5a0d
Itsakaseru <sakii@itsakaseru.me>
2024-06-30 00:56:37 +0700

    Remove cloudflare analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,12 +17,6 @@ export const metadata: Metadata = {
 export default function RootLayout({ children, }: Readonly<{ children: ReactNode; }>) {
   return (
     <html lang="en">
-    <Script
-      id="cloudflare-analytics"
-      defer
-      src="https://static.cloudflareinsights.com/beacon.min.js"
-      data-cf-beacon="{ 'token': '08c219abad4142a884e911161d2488e5' }"
-    />
     {/* Default Font: Geist Sans */}
     <body className={GeistSans.className}>
     {children}
