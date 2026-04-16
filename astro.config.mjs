b5e5fc0512b98ce55fb582d54ce0b53d7ab759bc
Lemuel Lancaster <sakii@itsakaseru.me>
2026-04-16 23:16:44 +0700

    Add required dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,17 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import tailwindcss from '@tailwindcss/vite';
+
+import mdx from '@astrojs/mdx';
+
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+    plugins: [tailwindcss()]
+  },
+
+  integrations: [mdx(), sitemap()]
+});
