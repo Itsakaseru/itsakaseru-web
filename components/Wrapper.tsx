168a39a413af3ca40268b3fb1546a0e419f4b189
Lemuel Lancaster <sakii@itsakaseru.me>
2026-04-16 21:43:26 +0700

    Reset for v5

--- components/Wrapper.tsx
+++
@@ -1,8 +0,0 @@
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import type { IconDefinition } from "@fortawesome/free-solid-svg-icons";
-
-export function FontAwesomeWrapper(icon: IconDefinition) {
-  return (
-    <FontAwesomeIcon icon={ icon } />
-  );
-}
--- libs/FileSystem.ts
+++
@@ -1,6 +0,0 @@
-import fs from "node:fs";
-
-export type fileType =
-  ".mdx";
-
-export function getFileList(folderPath: string, type: fileType) { return fs.readdirSync(folderPath, "utf-8").filter((file) => file.endsWith(type)); }
